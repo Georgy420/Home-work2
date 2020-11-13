d99227a73e2d3947006c0c05d265eb511362b5ca
Georgy <geo.dz420@gmail.com>
2020-11-13 12:39:19 +0300

    Home-work2GeekBrains

---
+++ 1_task.py
@@ -0,0 +1,7 @@
+my_list = [12, None, 'True', True, 9.5]
+def my_type(el):
+    for el in range(len(my_list)):
+        print(type(my_list[el]))
+
+my_type(my_list)
+
---
+++ 5_task.py
@@ -0,0 +1,14 @@
+my_list = [7, 5, 3, 3, 2]
+
+number = int(input("Введите число "))
+for el in range(len(my_list)):
+    if my_list[el] == number:
+        my_list.insert(el + 1, number)
+        break
+    elif my_list[0] < number:
+        my_list.insert(0, number)
+    elif my_list[-1] > number:
+        my_list.append(number)
+    elif my_list[el] > number and my_list[el + 1] < number:
+        my_list.insert(el + 1, number)
+print(f"Текущий список - {my_list}")
